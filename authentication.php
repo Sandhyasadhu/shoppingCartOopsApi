8794d192e017c9706ba24efa0bf291300ae68055
Sandhya <bs.sandhya.in@gmail.com>
2017-06-21 15:42:30 +0530

    update authenticate

--- authentication.php
+++ authentication.php
@@ -2,12 +2,12 @@
 class Authentication{
     public $username ;
 	function authenticate($username){
-		
-		$GLOBALS['db']->query("select id,username,role_id from users  where username='".$username."'  ");
-		$userDetails = $GLOBALS['db']->getrec();
-        $userId      = $userDetails['id'];
-        $roleId      = $userDetails['role_id'];
-		return $roleId;
+		$GLOBALS['db']->debug_mode=true;	
+	  $GLOBALS['db']->query("select id,username,role_id from users  where username='".$username."'  ");
+	  $userDetails = $GLOBALS['db']->getrec();
+          $userId      = $userDetails['id'];
+          $roleId      = $userDetails['role_id'];
+	  return $roleId;
 	}
 		
 }
