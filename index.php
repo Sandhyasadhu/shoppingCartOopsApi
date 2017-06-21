12e1e0573aeb74e267ea75e090c1822157353b6c
Sandhya <bs.sandhya.in@gmail.com>
2017-06-21 15:03:58 +0530

    index change

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<a href='/category.php?username=bs.sandhya.in&type=listCategories'>Category</a><br>";
-echo "<a href='/product.php?username=bs.sandhya.in&type=listCarts'>Product</a><br>";
+echo "<a href='/product.php?username=bs.sandhya.in&type=listProducts'>Product</a><br>";
 echo "<a href='/cart.php?username=bs.sandhya.in&type=listCarts'>Cart</a><br>";
 ?>
