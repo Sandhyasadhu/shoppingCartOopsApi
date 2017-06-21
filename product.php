a855095c0b66e0e4b9ebb163d8e4443da8d848ac
Sandhya <bs.sandhya.in@gmail.com>
2017-06-21 15:26:44 +0530

    change

--- product.php
+++ product.php
@@ -3,6 +3,7 @@ $incpath = dirname(__FILE__) . '/Product/';
 require_once($incpath . 'productDetails.php');
 require_once('authentication.php');
 require_once('db.php');
+$authenticate = new Authentication();
 $fileName     = basename(__FILE__, ".php");
 $fileName     = $fileName;
 $fileNameLast = substr($fileName, -1);
