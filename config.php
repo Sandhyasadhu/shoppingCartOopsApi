a1b534e7337f74f5442e14749212cb8a2fabe4ac
Sandhya <bs.sandhya.in@gmail.com>
2017-06-22 11:26:39 +0530

    config change

--- config.php
+++ config.php
@@ -7,10 +7,19 @@ ini_set("display_errors", 1);
 define("ADMIN","Administratie Panel");
 
 // -- DATABASE SETTINGS --
-define("DB_HOST","ec2-23-21-246-11.compute-1.amazonaws.com");
+$url = parse_url("postgres://vxhqizqwmfqzpo:d5249108238df98cc3412153f52fb556e3a84a32261e12d5bc7db8aae8130693@ec2-23-21-246-11.compute-1.amazonaws.com:5432/d1n4fevo38j8hs");
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+define("DB_HOST",$server);
+define("DB_USERNAME",$username);
+define("DB_PASSWORD",$password);
+define("DB_DATABASE",$db);
+/*define("DB_HOST","ec2-23-21-246-11.compute-1.amazonaws.com");
 define("DB_USERNAME","vxhqizqwmfqzpo");
 define("DB_PASSWORD",'d5249108238df98cc3412153f52fb556e3a84a32261e12d5bc7db8aae8130693');
-define("DB_DATABASE","d1n4fevo38j8hs");
+define("DB_DATABASE","d1n4fevo38j8hs");*/
 
 define('PERPAGE',9);
 define( "SEO" ,0);
